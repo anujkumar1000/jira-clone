f7594a631d0656a43dcd30654a7e8290ddbcdfc1
anuj <anujku10mar@gmail.com>
2024-11-04 14:08:06 +0530

    deployment

---
+++ app/(main)/organization/[orgId]/page.jsx
@@ -0,0 +1,43 @@
+import { getOrganization } from "@/actions/organization";
+import OrgSwitcher from "@/components/org-switcher";
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import ProjectList from "./_components/project-list";
+import UserIssues from "./_components/user-issues";
+
+const Organization = async ({ params }) => {
+  const { orgId } = params;
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const organization = await getOrganization(orgId);
+
+  if (!organization) {
+    return <div>Organization not found</div>;
+  }
+
+  return (
+    <div className="container mx-auto px-4">
+      <div className="mb-4 flex flex-col sm:flex-row justify-between items-start">
+        <h1 className="text-5xl font-bold gradient-title pb-2">
+          {organization.name}&rsquo;s Projects
+        </h1>
+
+        <OrgSwitcher />
+      </div>
+      <div className="mb-4">
+        <ProjectList orgId={organization.id} />
+      </div>
+      <div className="mt-8">
+        <UserIssues userId={userId} />
+        {/* <UserIssues userId={userId} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Organization;
